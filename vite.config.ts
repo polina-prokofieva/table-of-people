aaab727a857a4ebbbcaa7347ef45cd4eeff136a3
Polina Prokofieva <polina.prokofieva@gmail.com>
2023-03-21 09:56:12 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sassDts from 'vite-plugin-sass-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), sassDts()],
+  base: `/table-of-people/`,
+  define: {
+    PROJECT_NAME: JSON.stringify(process.env.npm_package_name),
+  },
+});
